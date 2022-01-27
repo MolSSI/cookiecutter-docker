896f2bd54d83434d45e252557da368fa3a199493
taylor-a-barnes <taylor.a.barnes@gmail.com>
2022-01-27 15:01:09 -0500

    Fix template image name

--- {{cookiecutter.repo_name}}/interactive.sh
+++ {{cookiecutter.repo_name}}/interactive.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo ${SCRIPT_DIR}
 cd ${SCRIPT_DIR}
 
-docker run --rm -v ${SCRIPT_DIR}:/repo -it molssi/bootcamp bash
+docker run --rm -v ${SCRIPT_DIR}:/repo -it {{ cookiecutter.image_name }} bash
--- {{cookiecutter.repo_name}}/run.sh
+++ {{cookiecutter.repo_name}}/run.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo ${SCRIPT_DIR}
 cd ${SCRIPT_DIR}
 
-docker run --rm -v ${SCRIPT_DIR}:/repo molssi/bootcamp bash docker/run.sh
+docker run --rm -v ${SCRIPT_DIR}:/repo {{ cookiecutter.image_name }} bash docker/run.sh
