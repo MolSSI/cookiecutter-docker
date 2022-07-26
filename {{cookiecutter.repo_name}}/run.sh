09b9a6c6b2a594979dff2caa40b782e95889b20e
Taylor Barnes <taylor.a.barnes@gmail.com>
2022-07-26 13:05:04 -0400

    Rename container_script.sh

--- {{cookiecutter.repo_name}}/run.sh
+++ {{cookiecutter.repo_name}}/run.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo ${SCRIPT_DIR}
 cd ${SCRIPT_DIR}
 
-sudo docker run --rm -v ${SCRIPT_DIR}:/repo {{ cookiecutter.image_name }} bash docker/run.sh
+sudo docker run --rm -v ${SCRIPT_DIR}:/repo {{ cookiecutter.image_name }} bash docker/container_script.sh
